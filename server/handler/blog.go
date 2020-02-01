c55e64852e0005af66e44b28cb44105b16809496
eavesmy <eavesmy@gmail.com>
2020-02-01 19:25:48 +0800

    up

---
+++ server/handler/blog.go
@@ -0,0 +1,15 @@
+package handler
+
+import (
+	"github.com/teambition/gear"
+)
+
+var Root = "/data/workspace/blogs"
+
+func List(ctx *gear.Context) error {
+	return nil
+}
+
+func Detail(ctx *gear.Context) error {
+	return nil
+}
--- server/main.go
+++ server/main.go
@@ -1,8 +1,22 @@
 package main
 
 import (
+	"./handler"
 	"github.com/teambition/gear"
 )
 
 func main() {
+	app := gear.New()
+	router := gear.NewRouter()
+
+	router.Get("/blog/list", handler.List)
+	router.Get("/blog/detail/:title", handler.Detail)
+
+	app.Use(func(ctx *gear.Context) error {
+		return nil
+	})
+
+	app.UseHandler(router)
+
+	app.Listen(":8080")
 }
